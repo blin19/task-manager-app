70722303db33f1d1edec92c909b85dbe91f58bfe
Brianna Lin <bjlin19@seas.upenn.edu>
2025-09-12 16:27:22 -0400

    Create TaskItem.js

---
+++ client/src/components/TaskItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TaskItem = ({ task, onToggle, onDelete }) => {
+  return (
+    <li className={`task ${task.completed ? 'completed' : ''}`}>
+      <span onClick={() => onToggle(task._id, !task.completed)}>{task.title}</span>
+      <button onClick={() => onDelete(task._id)}>🗑</button>
+    </li>
+  );
+};
+
+export default TaskItem;
