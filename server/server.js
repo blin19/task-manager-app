97cf3c9c957377ab0cce8d8ab9e30d613faf6f97
Brianna Lin <bjlin19@seas.upenn.edu>
2025-09-12 16:24:02 -0400

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const taskRoutes = require('./routes/tasks');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/tasks', taskRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(5000, () => console.log('Server running on port 5000'));
+  })
+  .catch(err => console.error(err));
